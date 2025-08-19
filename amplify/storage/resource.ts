2fdb8e86ea2f94838f8c1892b644bfb2afd055d3
kshiina-r <kenta.shiina@jp.ricoh.com>
2025-08-19 23:56:12 +0900

    resourceの追加

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -9,3 +9,27 @@ export const storage = defineStorage({
     ],
   }),
 });
+
+export const firstBucket = defineStorage({
+  name: 'firstBucket',
+  isDefault: true, // identify your default storage bucket (required)
+});
+
+export const secondBucket = defineStorage({
+  name: 'secondBucket',
+  access: (allow) => ({
+    'private/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
+  })
+})
+
+export const thirdBucket = defineStorage({
+  name: 'thirdBucket',
+  access: (allow) => ({
+    'public/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),
+    ],
+  }),
+});
